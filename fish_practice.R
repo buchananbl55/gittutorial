17677cde27fb20f8b557cbed3276c86728f398aa
Brittaney Buchanan <buchananbl55@gmail.com>
2018-10-19 11:40:02 -0500

    edit

--- fish_practice.R
+++ fish_practice.R
@@ -4,7 +4,7 @@ source("R/Personal_Themes.R") # automatically adds personal themes to environmen
 
 fish <- read_csv("data/Gaeta_etal_CLC_data.csv") # load fish data
 
-# add column with distinction based on length >=200
+# add column with distinction based on length >=300
 fish <- fish %>%
   mutate(length_cat = case_when(length >= 300 ~ "big",
                                 length < 300 ~ "small"))
