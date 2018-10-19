a443f2603c6bf2edac9d22eb0f2760532720db78
Brittaney Buchanan <buchananbl55@gmail.com>
2018-10-19 11:33:33 -0500

    changed cutoff to 300

--- fish_practice.R
+++ fish_practice.R
@@ -6,6 +6,7 @@ fish <- read_csv("data/Gaeta_etal_CLC_data.csv") # load fish data
 
 # add column with distinction based on length >=200
 fish <- fish %>%
-  mutate(length_cat = case_when(length >= 200 ~ "big",
-                                length < 200 ~ "small"))
+  mutate(length_cat = case_when(length >= 300 ~ "big",
+                                length < 300 ~ "small"))
 
+head(fish)
