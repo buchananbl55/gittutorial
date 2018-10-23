a936b40d203ad2ec600072936ca76b1cf1e69c09
Christopher Chizinski <chris.chizinski@gmail.com>
2018-10-23 13:38:34 -0500

    added another plot with some modifications

--- fish_practice.R
+++ fish_practice.R
@@ -20,3 +20,13 @@ ggplot() +
   theme_britt() +
   scale_fill_hp(discrete = TRUE, house = "slytherin")
 
+## Chris's edits
+
+ggplot() +
+  geom_histogram(data = fish, aes(x = scalelength, fill = length_cat), colour = "black", bins = 25) +
+  facet_wrap(~ length_cat) +
+  coord_cartesian(ylim = c(0,500), xlim = c(0, 11), expand = FALSE) +
+  theme_classic() +
+  scale_fill_hp(discrete = TRUE, house = "slytherin") +
+  theme(legend.position = "none",
+        panel.spacing = unit(2, "lines"))
